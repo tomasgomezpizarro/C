69626106f414a314f75139d877afbe39a6e7d054
tomasgomezpizarro <tomasgomezpizarro1907@gmail.com>
2025-03-07 09:02:23 -0300

    kickstart 6 y 7

---
+++ labs/lab7/kickstart/ej2/dijkstra.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#include "cost.h"
+#include "graph.h"
+#include "mini_set.h"
+
+
+cost_t *dijkstra(graph_t graph, vertex_t init) {
+    /*
+     * TODO: COMPLETAR
+     *
+     */
+}
